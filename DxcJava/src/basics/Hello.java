a0c3be3152fc0518d2d1e05b2b19a1ac1cc531d0
abdul <abdul.tanveer@gmail.com>
2021-10-19 12:12:37 +0530

    student class

--- DxcJava/src/basics/Hello.java
+++ DxcJava/src/basics/Hello.java
@@ -1,7 +1,19 @@
 package basics;
 
 public class Hello {
+	//age is an instance[box] variable -- partition
+	int age = 20;
+	
+	public static String COLLEGE = "mit";
+	
+	
 	public static void main(String[] args) {
 		System.out.println("hello world");
+	}
+	
+	int add() {
+		//sName is a local variable -- within a function
+		String sName = "abdul";
+		return 18;
 	}
 }
---
+++ DxcJava/src/basics/Student.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class Student {
+	
+	String name; //partition 
+	int age; // 
+	
+	static String COLLEGE_NAME = "IIT";
+	
+	//Student[] students = new Student[100];
+
+}

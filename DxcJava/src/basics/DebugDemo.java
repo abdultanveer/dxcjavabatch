69cce6449765d109e63800741b704f8842d0a9b4
abdul <abdul.tanveer@gmail.com>
2021-10-19 15:04:02 +0530

    writing logs

--- DxcJava/src/basics/DebugDemo.java
+++ DxcJava/src/basics/DebugDemo.java
@@ -3,6 +3,7 @@ package basics;
 public class DebugDemo {
 	
 	public static void main(String[] args) {
+		System.out.println("DebugDemo-- main");
 		Calculator calc = new Calculator();
 		int sum = calc.add(10, 20);
 		System.out.println("the sum is--"+sum);

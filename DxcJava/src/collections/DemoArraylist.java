07b85e7099b1796a1b1352df19f9319400d9b2df
abdul <abdul.tanveer@gmail.com>
2021-10-26 10:54:00 +0530

    arraylist

---
+++ DxcJava/src/collections/DemoArraylist.java
@@ -0,0 +1,15 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoArraylist {
+	
+	public static void main(String[] args) {
+		List<String> names = new ArrayList<String>();
+		names.add("abdul");
+		//names.add(30);
+		System.out.println(names);
+	}
+
+}

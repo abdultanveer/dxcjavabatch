733319dfe61f707d36c5de70a76ef92289f7aa78
abdul <abdul.tanveer@gmail.com>
2021-10-27 10:20:14 +0530

    collections and unit tests

--- DxcJava/src/abstraction/BmwFlyingCar.java
+++ DxcJava/src/abstraction/BmwFlyingCar.java
@@ -5,19 +5,29 @@ public class BmwFlyingCar extends FlyingCar{
 	@Override
 	public void stopCar() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void flyCar() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void landCar() {
 		// TODO Auto-generated method stub
-		
+
 	}
+
+	public static	int add(int a, int b) {
+		return a+b;
+	}
+
+	public static boolean isEligibletoVote(int age) {
+		if (age >17) {return true;}
+		return false;
+	}
+
 
 }

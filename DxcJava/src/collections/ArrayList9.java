449ac467e709a1e172593c6d7aef11cf4ddadfdf
abdul <abdul.tanveer@gmail.com>
2021-10-27 16:43:37 +0530

    generics

--- DxcJava/src/collections/ArrayList9.java
+++ DxcJava/src/collections/ArrayList9.java
@@ -3,13 +3,17 @@ import java.util.*;
 class ArrayList9{  
 	public static void main(String args[]){  
 		ArrayList<String> al=new ArrayList<String>();  
-		al.add("Ravi");  
+		al.add("Ravi");
+		//al.add(10);
 		al.add("Vijay");  
 		al.add("Ajay");  
-		ArrayList<String> al2=new ArrayList<String>();  
+		ArrayList<String> al2=new ArrayList<String>();  //generic arraylist
 		al2.add("Ravi");  
 		al2.add("Hanumat"); 
 		al2.add("Ajay");  
+		String myname = al2.get(0);
+
+		//ArrayList<Object> al = new ArrayList<Boolean>();
 
 		al.retainAll(al2); 
 

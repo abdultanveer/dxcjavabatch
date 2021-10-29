8ab07c49200adb31755b3467073b203087455efb
abdul <abdul.tanveer@gmail.com>
2021-10-29 12:35:41 +0530

    factory design pattern for dependency injection

---
+++ DxcJava/src/dependency/injection/JioService.java
@@ -0,0 +1,11 @@
+package dependency.injection;
+
+public class JioService implements Service{
+
+	@Override
+	public String getInfo() {
+		// TODO Auto-generated method stub
+		return "jio services";
+	}
+
+}
---
+++ DxcJava/src/dependency/injection/VodafoneService.java
@@ -0,0 +1,11 @@
+package dependency.injection;
+
+public class VodafoneService implements Service {
+
+	@Override
+	public String getInfo() {
+		// TODO Auto-generated method stub
+		return "vodafone service";
+	}
+
+}
